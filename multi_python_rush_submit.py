7330317ac9de12c600503d8260b83fa6b0971f47
Matt Carroll <matt.carroll.12129@gmail.com>
2018-05-09 11:32:58 -0400

    commit submission file

---
+++ multi_python_rush_submit.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+if (len(sys.argv) <= 1):
+	if (os.environ.has_key("RUSH_ISDAEMON")):
+		sys.exit(1)
+
+submit = os.popen("rush -submit", "w")
+
+submit.write(	 "title				%s\n"							
+				+"ram				1024\n " 									
+				+"frames			%s\n"									
+				+"logdir			/hd/na10/junk\n"							
+				+"command			%s\n"
+				+"donemail			mcarroll@amnh.org\n"									
+				+"waitforstate			done\n"													
+				+"cpus				linuxbox32\n"													
+				 #"nevercpus			#NEVERCPUS\n"												 
+			) % ("MYTITLE", "MYFRAMES", "MYCOMMAND")
+
+err = submit.close()
+if err:
+	sys.exit(1)			#Submit Failed 
+sys.exit(0)				#Submit Succeeded 
