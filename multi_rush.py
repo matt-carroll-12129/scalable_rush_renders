1c753746180c44467a8ba5d3ee312382d5bede0a
Matt Carroll <matt.carroll.12129@gmail.com>
2018-05-10 12:18:22 -0400

    give user choice to name new rush_script_dir

--- multi_rush.py
+++ multi_rush.py
@@ -8,10 +8,17 @@ import subprocess
 time = datetime.datetime.now()
 now = (time.strftime("%Y%m%d"))
 job_num = 0
+command_files = []
 
 # Make a new directory to house our bundle of scripts we're about to create. The new folder will be named with the date on the end. 
 user_rushscript_dir = "/Users/matt/Documents/python/scalable_rush_renders"
-new_rushscript_dir = os.path.join(user_rushscript_dir + "/nsd_" + now)
+while True:
+	try:
+		nsd = input("New directory for these scripts will be created here {}. Name the directory without using the date and surround in quotations ".format(user_rushscript_dir))
+		break
+	except (NameError, SyntaxError) as e:
+		print("ERROR: Use quotations. Try again.")
+new_rushscript_dir = os.path.join(user_rushscript_dir + "/" + nsd + "_" + now)
 os.umask(000)
 if not os.path.isdir(new_rushscript_dir):
 	os.mkdir(new_rushscript_dir, 0777)
@@ -27,6 +34,7 @@ for i in range(user_input):
 	old_command_file = os.path.join(new_rushscript_dir + "/multi_HOPE_ClimateWall")
 	new_command_file = os.path.join(new_rushscript_dir + "/HOPE_ClimateWall_" + str(i+1))
 	os.rename(old_command_file, new_command_file)
+	command_files.append(new_command_file)
 
 	# Create new submit file for each job to be rendered.
 	shutil.copy("multi_python_rush_submit.py", new_rushscript_dir)
@@ -42,8 +50,8 @@ for i in range(user_input):
 			frames = input("[JOB #{}] What is the frame range? Format = X-XXX. Put in quotations ".format(job_num))
 			command = new_command_file
 			break 
-		except NameError:
-			print("ERROR: Use quotations. Start Again.")
+		except (NameError, SyntaxError) as e:
+			print("ERROR: Use quotations. Start again.")
 	# Customize individual command file. The directory tree should be MYFRAME/src_frames/MRFRAME.12345.png. Name would then be MYFRAME.
 	with open(new_command_file, "r") as f:
 		file = f.read()
@@ -71,6 +79,11 @@ for i in range(user_input):
 	with open(new_submit_file, "w") as f:
 		f.write(file)
 		f.close()
+
+# Execute Commands 
+for command_file in command_files: 
+	print(command_file)
+
 
 		
 
