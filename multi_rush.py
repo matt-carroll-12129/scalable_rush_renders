067be95530db53a14710f8ff58f930f8dbc7ee80
Matt Carroll <matt.carroll.12129@gmail.com>
2018-05-21 13:47:24 -0400

    changed structure + added render file

--- multi_rush.py
+++ multi_rush.py
@@ -10,11 +10,13 @@ now = (time.strftime("%Y%m%d")) 					# For tagging new_rushscript_dir with date
 job_num = 0 										# For tagging the user prompts with a job #. 
 command_files = [] 									# The final product 
 submit_files = []									# The final product 
+render_files = []									# The final product
 
 
 # Make a new directory to house our bundle of scripts we're about to create. The new folder will be named with the date on the end. 
 # Ask user how many jobs there are to render. This value is thrown into a for loop to create the appropriate number of files. 
 user_rushscript_dir = "/Users/matt/Documents/python/scalable_rush_renders"
+
 while True:
 	try:
 		nsd = input("New directory for these scripts will be created here {}. Name the directory without using the date and surround in quotations. ".format(user_rushscript_dir))
@@ -30,19 +32,27 @@ if not os.path.isdir(new_rushscript_dir):
 
 for i in range(int(job_count)):
 	job_num += 1
-	# Create new command file for each job to be rendered.
+	# Create new command file for each job.
 	shutil.copy("multi_HOPE_ClimateWall", new_rushscript_dir)
 	old_command_file = os.path.join(new_rushscript_dir + "/multi_HOPE_ClimateWall")
 	new_command_file = os.path.join(new_rushscript_dir + "/HOPE_ClimateWall_" + str(i+1))
 	os.rename(old_command_file, new_command_file)
 	command_files.append(new_command_file)
 
-	# Create new submit file for each job to be rendered.
+	# Create new submit file for each job.
 	shutil.copy("multi_python_rush_submit.py", new_rushscript_dir)
 	old_submit_file = os.path.join(new_rushscript_dir + "/multi_python_rush_submit.py")
 	new_submit_file = os.path.join(new_rushscript_dir + "/multi_python_rush_submit_" + str(i+1) + ".py")
 	os.rename(old_submit_file, new_submit_file)
 	submit_files.append(new_submit_file)
+
+	# Create new render file for each job. 
+	shutil.copy("multi_HOPE_ClimateWall_render", new_rushscript_dir)
+	old_render_file = os.path.join(new_rushscript_dir + "/multi_HOPE_ClimateWall_render")
+	new_render_file = os.path.join(new_rushscript_dir + "/multi_HOPE_ClimateWall_render_" + str(i+1))
+	os.rename(old_render_file, new_render_file)
+	render_files.append(new_render_file)
+
 
 	# Get user input for variables to be used in new files. 	
 	while True:	
@@ -50,7 +60,7 @@ for i in range(int(job_count)):
 			name = input("[JOB #{}] What is the frame name root? ($idir/$name.$i.$iext). Put inside quotations ".format(job_num))
 			title = input("[JOB #{}] What would you like to name your render? Put inside quotations ".format(job_num))
 			frames = input("[JOB #{}] What is the frame range? Format = X-XXX. Put in quotations ".format(job_num))
-			command = new_command_file
+			command = new_render_file
 			break 
 		except (NameError, SyntaxError) as e:
 			print("ERROR: Use quotations. Start again.")
@@ -82,12 +92,23 @@ for i in range(int(job_count)):
 		f.write(file)
 		f.close()
 
+	# Customize individual render file. 
+	with open(new_render_file, "r") as f:
+		file = f.read()
+	file = file.replace("new_command_file", new_command_file)
+	with open(new_render_file, "w") as f:
+		f.write(file)
+		f.close()
+
 # Execute Commands 
 for command_file in command_files: 
 	print("COMMAND FILES CREATED:: " + command_file)
 
 for submit_file in submit_files:
 	print("SUBMIT FILES CREATED:: " + submit_file)
+
+for render_file in render_files:
+	print("RENDER FILES CREATED:: " + render_file)
 
 		
 
